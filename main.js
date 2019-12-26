7a0577f80fad8712b68c582df624ac3b62734796
ivanbiljan <ssbiljan@gmail.com>
2019-12-26 14:06:15 +0100

    Restructure files

--- main.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var Bot = require("./framework/DiscordBot");
-var config = require('./config/config.json');
-var bot = new Bot.DefaultBot(config);
-bot.connect();
