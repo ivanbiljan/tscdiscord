9173d68a53895ab780f725f859cba799aa0b09e8
ivanbiljan <ssbiljan@gmail.com>
2019-12-26 18:55:14 +0100

    Update travis.yml

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import * as Bot from './framework/DiscordBot';
 import { ConfigurationFile } from './config/ConfigurationFile';
 
-const config: ConfigurationFile = require('./config/config.json');
+const config: ConfigurationFile = require('../config.json');
 const bot: Bot.DefaultBot = new Bot.DefaultBot(config);
 bot.connect();
